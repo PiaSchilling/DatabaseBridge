3bbe7587806311caba1881339f143c2581612f7b
kn022 <kn022@hdm-stuttgart.de>
2023-12-01 16:09:58 +0100

    fix(3): rename schema to database

--- project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/connect/ConnectionDetails.java
+++ project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/connect/ConnectionDetails.java
@@ -4,7 +4,7 @@ public class ConnectionDetails {
     private final DatabaseSystem databaseSystem;
     private final String hostAddress;
     private final int port;
-    private final String schema;
+    private final String database;
     private final String username;
     private final String password;
     private String jdbcUri;
@@ -14,15 +14,15 @@ public class ConnectionDetails {
      * @param databaseSystem the DatabaseSystem type
      * @param hostAddress (weiß nicht genau was das ist bitte selbst ausfüllen)
      * @param port port number, on which the database is accessible 
-     * @param schema (weiß nicht genau was/wozu das ist bitte selbst ausfüllen)
+     * @param database (weiß nicht genau was/wozu das ist bitte selbst ausfüllen)
      * @param username username to login into the database
      * @param password passwort to login into the database
      */
-    public ConnectionDetails(DatabaseSystem databaseSystem, String hostAddress, int port, String schema, String username, String password) {
+    public ConnectionDetails(DatabaseSystem databaseSystem, String hostAddress, int port, String database, String username, String password) {
         this.databaseSystem = databaseSystem;
         this.hostAddress = hostAddress;
         this.port = port;
-        this.schema = schema;
+        this.database = database;
         this.username = username;
         this.password = password;
         this.jdbcUri = this.createJdbcUri();
@@ -36,13 +36,13 @@ public class ConnectionDetails {
         jdbcUri = "jdbc:";
         switch (this.databaseSystem) {
             case MYSQL:
-                jdbcUri = jdbcUri + "mysql://" + this.hostAddress + ":" + this.port + "/" + this.schema;
+                jdbcUri = jdbcUri + "mysql://" + this.hostAddress + ":" + this.port + "/" + this.database;
                 break;
             case MARIADB:
-                jdbcUri = jdbcUri + "mariadb://" + this.hostAddress + ":" + this.port + "/" + this.schema;
+                jdbcUri = jdbcUri + "mariadb://" + this.hostAddress + ":" + this.port + "/" + this.database;
                 break;
             case POSTGRES:
-                jdbcUri = jdbcUri + "postgresql://" + this.hostAddress + ":" + this.port + "/" + this.schema;
+                jdbcUri = jdbcUri + "postgresql://" + this.hostAddress + ":" + this.port + "/" + this.database;
         }
 
         return jdbcUri;
