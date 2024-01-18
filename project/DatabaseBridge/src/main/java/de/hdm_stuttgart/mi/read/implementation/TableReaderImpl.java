b8db718c76e73e9175b4b30c5e34c6f22ef10078
Pia Schilling <ps149@hdm-stuttgart.de>
2024-01-18 17:44:29 +0100

    feat(#4): add delete and update rules

--- project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/read/implementation/TableReaderImpl.java
+++ project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/read/implementation/TableReaderImpl.java
@@ -5,6 +5,7 @@ import com.google.inject.name.Named;
 import de.hdm_stuttgart.mi.read.api.ColumnReader;
 import de.hdm_stuttgart.mi.read.api.TableReader;
 import de.hdm_stuttgart.mi.read.model.Column;
+import de.hdm_stuttgart.mi.read.model.DeleteUpdateRule;
 import de.hdm_stuttgart.mi.read.model.FkRelation;
 import de.hdm_stuttgart.mi.read.model.Table;
 
@@ -44,19 +45,28 @@ public class TableReaderImpl implements TableReader {
 
     /**
      * Read the foreign key relations where this table references other tables primary keys (this table is the child)
+     *
      * @param tableName the name of the table of which the imported fk relations should be read
      * @return a list of the imported fk relations
      */
     private ArrayList<FkRelation> readImportedFkRelations(String tableName) {
         final ArrayList<FkRelation> importedFkRelations = new ArrayList<>();
         try {
-            final ResultSet importedKeys = metaData.getImportedKeys(null,null,tableName);
-            while (importedKeys.next()){
+            final ResultSet importedKeys = metaData.getImportedKeys(null, null, tableName);
+            while (importedKeys.next()) {
                 final String referencedTableName = importedKeys.getString("PKTABLE_NAME");
                 final String referencedColumnName = importedKeys.getString("PKCOLUMN_NAME");
+                final short updateRule = importedKeys.getShort("UPDATE_RULE");
+                final short deleteRule = importedKeys.getShort("DELETE_RULE");
                 final String fkName = importedKeys.getString("FK_NAME");
 
-                importedFkRelations.add(new FkRelation(referencedTableName,referencedColumnName,fkName));
+
+                importedFkRelations.add(new FkRelation(
+                        referencedTableName,
+                        referencedColumnName,
+                        fkName,
+                        DeleteUpdateRule.fromCode(updateRule),
+                        DeleteUpdateRule.fromCode(deleteRule)));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -67,21 +77,29 @@ public class TableReaderImpl implements TableReader {
 
     /**
      * Read the foreign key relations where this tables primary key is referenced by other tables (this table is the parent)
+     *
      * @param tableName the name of the table of which the exported fk relations should be read
      * @return a list of the exported fk relations
      */
     private ArrayList<FkRelation> readExportedFkRelations(String tableName) {
         final ArrayList<FkRelation> importedFkRelations = new ArrayList<>();
         try {
-            final ResultSet importedKeys = metaData.getExportedKeys(null,null,tableName);
-            while (importedKeys.next()){
+            final ResultSet importedKeys = metaData.getExportedKeys(null, null, tableName);
+            while (importedKeys.next()) {
                 final String referencingTableName = importedKeys.getString("FKTABLE_NAME");
                 final String referencingColumnName = importedKeys.getString("FKCOLUMN_NAME");
+                final short updateRule = importedKeys.getShort("UPDATE_RULE");
+                final short deleteRule = importedKeys.getShort("DELETE_RULE");
                 final String fkName = importedKeys.getString("FK_NAME");
 
                 final String thisTableName = importedKeys.getString("FKTABLE_NAME");
 
-                importedFkRelations.add(new FkRelation(referencingTableName,referencingColumnName,fkName));
+                importedFkRelations.add(new FkRelation(
+                        referencingTableName,
+                        referencingColumnName,
+                        fkName,
+                        DeleteUpdateRule.fromCode(updateRule),
+                        DeleteUpdateRule.fromCode(deleteRule)));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/read/model/FkRelation.java
+++ project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/read/model/FkRelation.java
@@ -6,6 +6,9 @@ public class FkRelation {
     private final String tableName;
     private final String columnName;
 
+    private final DeleteUpdateRule updateRule;
+    private final DeleteUpdateRule deleteRule;
+
 
     /**
      * Models a foreign key relationship to a column of a table
@@ -13,11 +16,15 @@ public class FkRelation {
      * @param tableName  the name of the table this FkRelation references
      * @param columnName the name of the column this FkRelation references
      * @param fkName     the name of the foreign key
+     * @param updateRule what happens to a foreign key when the primary key is updated
+     * @param deleteRule What happens to a foreign key when the primary key is deleted
      */
-    public FkRelation(String tableName, String columnName, String fkName) {
+    public FkRelation(String tableName, String columnName, String fkName, DeleteUpdateRule updateRule, DeleteUpdateRule deleteRule) {
         this.tableName = tableName;
         this.columnName = columnName;
         this.fkName = fkName;
+        this.updateRule = updateRule;
+        this.deleteRule = deleteRule;
     }
 
     @Override
@@ -26,6 +33,8 @@ public class FkRelation {
                 "fkName='" + fkName + '\'' +
                 ", tableName='" + tableName + '\'' +
                 ", columnName='" + columnName + '\'' +
+                ", updateRule=" + updateRule +
+                ", deleteRule=" + deleteRule +
                 '}';
     }
 }
