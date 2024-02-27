9c7ee63c11d6a7ed9b3b49eb4ee981ff316763f0
Pia Schilling <ps149@hdm-stuttgart.de>
2024-02-27 15:56:09 +0100

    refactor(#7): add test main method

--- project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/Main.java
+++ project/DatabaseBridge/src/main/java/de/hdm_stuttgart/mi/Main.java
@@ -7,8 +7,10 @@ import de.hdm_stuttgart.mi.connect.model.ConnectionDetails;
 import de.hdm_stuttgart.mi.connect.model.DatabaseSystem;
 import de.hdm_stuttgart.mi.di.ConnectModule;
 import de.hdm_stuttgart.mi.di.SchemaReadModule;
-import de.hdm_stuttgart.mi.read.api.SchemaReader;
-import de.hdm_stuttgart.mi.read.model.*;
+import de.hdm_stuttgart.mi.read.schema.api.SchemaReader;
+import de.hdm_stuttgart.mi.read.schema.model.Schema;
+import de.hdm_stuttgart.mi.util.DbSysConstsLoader;
+import de.hdm_stuttgart.mi.write.schema.SchemaWriter;
 
 
 public class Main {
@@ -37,7 +39,7 @@ public class Main {
                 "src/main/resources/jar/postgresql-42.7.1.jar",
                 "localhost",
                 5432,
-                "test",
+                "ecommerce",
                 "postgres",
                 "example");
 
@@ -51,13 +53,34 @@ public class Main {
                 "postgres",
                 "example");
 
-        Injector injector = Guice.createInjector(new ConnectModule(sourceDetailsPostgres, destinationDetailsPostgres),
+        // TODO move logic to controller once finished testing
+        final ConnectionDetails testSourceDetails = sourceDetailsMySql;
+
+
+        Injector injector = Guice.createInjector(new ConnectModule(testSourceDetails, destinationDetailsPostgres),
                 new SchemaReadModule());
+        DbSysConstsLoader.INSTANCE().init(testSourceDetails.getDatabaseSystem());
 
         SchemaReader schemaReader = injector.getInstance(SchemaReader.class);
-        final Schema schema = schemaReader.readSchema(sourceDetailsPostgres.getSchema());
+        final Schema schema = schemaReader.readSchema(testSourceDetails.getSchema());
 
-        System.out.println(schema);
+        // System.out.println(schema);
+
+        SchemaWriter schemaWriter = new SchemaWriter();
+        schemaWriter.writeSchema(schema);
+
+
+     /*   final ArrayList<Constraint> c = new ArrayList();
+        final Constraint constraint = new Constraint(ConstraintType.NOT_NULL);
+        final Constraint constraint2 = new Constraint(ConstraintType.DEFAULT, "test");
+        c.add(constraint);
+        c.add(constraint2);
+
+        final Column test = new Column("user", SQLType.VARCHAR, 64, c);
+        System.out.println(test.asStatement());
+
+        final FkRelation relation = new FkRelation("users", "id", "user_idRef", DeleteUpdateRule.IMPORTED_KEY_CASCADE, DeleteUpdateRule.IMPORTED_NO_ACTION);
+        System.out.println(relation.asStatement());*/
 
         // TODO close DB connection
     }
